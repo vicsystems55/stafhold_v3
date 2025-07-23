eae6e19df3ab29c51014eedf0cb5124340686426
vicsystems <victorasuquob@gmail.com>
2025-07-23 17:43:44 +0100

    too long 1071 -error

---
+++ resources/views/pages/subscribe-success.blade.php
@@ -0,0 +1,9 @@
+// resources/views/subscribe/success.blade.php
+@extends('layouts.base')
+
+@section('content')
+    <div class="container text-center py-5">
+        <h1>Thank you for subscribing!</h1>
+        <p>You’ve successfully joined our newsletter.</p>
+    </div>
+@endsection
